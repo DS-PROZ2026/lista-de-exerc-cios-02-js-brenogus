118ae6887b46a5907c4abff5c361c24c132bf481
brenogus <brenodjsk86@gmail.com>
2026-05-07 19:22:49 -0300

    Add files via upload
    ex 11 correto, mandei um teste sem querer antes.

--- ex11.js
+++ ex11.js
@@ -3,6 +3,29 @@ let nomes = [];
 
 dados = prompt("Digite os nomes separados por '' (espaço): ");
 
-nomes = dados.split(" ").map((nome,i) => nome = nome.trim());
 
-nomes.forEach((nome,i) => console.log(nome))
+nomes = dados.split(" ").map((nome,i) => nome = nome.trim());
+nomes = nomes.map((nome) => nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase());
+let contador = 0;
+let nome;
+
+do {
+    
+    if (contador < nomes.length) {
+        
+        
+        if (nomes[contador] === "Ricardo") {
+            break; 
+        }
+        
+        contador++; 
+    } else {
+        console.log("Usuário não encontrado!");
+        break;
+    }
+} while (true); 
+
+
+if (nomes[contador] === "Ricardo") {
+    console.log(`Usuário ${nomes[contador]} encontrado!`);
+}
